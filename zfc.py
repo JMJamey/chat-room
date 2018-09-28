20a795767a95853773f2f8032d7473b5dde23aae
cjm <tarena@du.com>
2018-09-28 21:05:08 +0800

    a file

---
+++ zfc.py
@@ -0,0 +1,21 @@
+#a = "hello world"
+#b = "abcd"
+#c = 'a'
+#d = "%20s" % a
+#print(d) 
+#print("%20s" % b)
+#print("%20s" % c)
+
+#max_len = len(a)
+# if len(b)>max_len:
+#     max_len = len(b)
+# if len(c)>max_len:
+#     max_len = len(c)
+#print(" "*(max_len - len(a)+a))
+#print(" "*(max_len - len(b)+b))
+#print(" "*(max_len - len(c)+c))
+
+# max_len = max(len(a),len(b),len(c))
+# print("",max_len)
+# print("%max_lens" % a)
+
